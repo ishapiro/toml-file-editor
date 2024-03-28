f5ffdaef02894251fd4b810119ef68eba8c2590f
Irv Shapiro <ishapiro@cogitations.com>
2024-03-28 18:05:07 -0500

    Basic functions working. Need to work on help windows, etc.

--- toml-editor-module.py
+++ toml-editor-module.py
@@ -1,19 +1,21 @@
 # Forked from https://github.com/jhaakma/toml-editor
 # Completely rewritten to prompt user for file to process and save to a new or existing file
+# Integrated ttkbootstrap for a more modern look
 
 import os
 import sys
 from tkinter import *
 from tkinter.filedialog import askopenfilename
 from tkinter.ttk import *
+import ttkbootstrap as ttk
+from ttkbootstrap.constants import *
 import toml, json
 
-
+# Global Variables
 title = "Benchmark Configuration Editor"
-window_size = "1000x900"
-
-global toml_path, config, toml_settings, fields, path
-
+toml_path = ""
+config = {}
+fields = {}
 toml_settings = {}
 
 def process_file():
@@ -27,47 +29,26 @@ def process_file():
         elif toml_path.endswith('.json'):
             config = json.load(f)
 
-    load_editor(config, fields, inner_frame)
-
-def load_editor(config, fields, inner_frame):
-
+    # Clear the help text
+    #help_frame.destroy()
+    
+    # Put the fields on the screen
     render_settings(config, fields, inner_frame)
 
-    frame.pack(side="top", fill="both", expand=True)
-    canvas.pack(side="top", fill="both", expand=True)
-    scrollbar.pack(side="right", fill="y")
-
-    # Create a frame to hold the save button
-    button_frame = Frame(frame)
-    button_frame.pack(side="bottom", pady=10)
-
-    save_button = Button(button_frame, text="Update Configuration", command=save_settings)
-    save_button.pack()
-
-    new_label = Label(button_frame, text="New File Path")
-    new_label.pack()
-
-    new_filename = Text(button_frame, width=100, height=5)
-    new_filename.insert(END, path)
-    new_filename.pack()
-
-    new_button = Button(button_frame, text="New Configuration at New File Path", command=new_settings)
-    new_button.pack()
 
 def get_settings_path():
-    # Check if the script is running from a PyInstaller bundle
-    # if getattr(sys, 'frozen', False):
-    #     # Return the path to the bundled settings.toml
-    #     return os.path.join(sys._MEIPASS, 'settings.toml')
-    # else:
-    #     # Return the path to the settings.toml in the source directory
-    #     return os.path.abspath(os.path.join(os.path.dirname(__file__), 'settings.toml'))
     return askopenfilename()    
 
 def get_settings():
     try:
         with open(get_settings_path(), 'r') as f:
             toml_settings = toml.load(f)
+            toml_path = f.name
+            new_filename.delete("1.0", 'end')  # Delete existing text from the beginning to the end
+            new_filename.insert("1.0", toml_path)  # Insert new text at the beginning
+            original_path.set(toml_path)
+            new_button["state"] = "enabled"
+            save_button["state"] = "enabled"
             return toml_settings, f.name
     except FileNotFoundError:
         return toml_settings
@@ -125,8 +106,6 @@ def render_settings(current_settings, current_fields, parent_frame):
             else:
                 # set height based on number of characters plus some padding
                 height = int(len(str(value)) / 40) + 1
-
-
                 current_fields[key] = value
                 text = Text(parent_frame, width=50, height=height)
                 text.insert(END, value)
@@ -151,36 +130,63 @@ def validate_number_input(input_value):
 
 # Update the TOML file with the new settings
 def save_settings():
-    with open(path, "w") as f:
-        if path.endswith('.toml'):
+    save_path = original_path.get()
+    with open(save_path, "w") as f:
+        if f.name.endswith('.toml'):
             toml.dump(fields, f)
-        elif path.endswith('.json'):
+        elif f.name.endswith('.json'):
             json.dump(fields, f, indent=4)
         else:
             raise Exception("Invalid file type")
-    root.destroy()
+        
+    clear_interframe()
         
 # Update the TOML file with the new settings
 def new_settings():
     new_path = new_filename.get("1.0", "end-1c")
-    print(f"File name for new configuration: {new_path}")
+    print(f"File name for new configuration: {new_path}||")
     with open(new_path, "w") as f:
-        if path.endswith('.toml'):
+        if f.name.endswith('.toml'):
             toml.dump(fields, f)
-        elif path.endswith('.json'):
+        elif f.name.endswith('.json'):
             json.dump(fields, f, indent=4)
         else:
             raise Exception("Invalid file type")
-    root.destroy()
+        
+    clear_interframe()
 
-root = Tk()
+def process_all_files():
+    return
+
+def process_this_configuration():
+    return
+
+def clear_parameters():
+    return
+
+def clear_interframe():
+    for widget in inner_frame.winfo_children():
+        widget.destroy()
+
+    # Disable the buttons until we load another file
+    new_button["state"] = "disabled"
+    save_button["state"] = "disabled"
+
+    # Clear the file name fields
+    new_filename.delete("1.0", 'end')  # Delete existing text from the beginning to the end
+    original_path.set("")
+
+
+#########################################################
+# Main Code Starts Here
+#########################################################
+
+root = ttk.Window(themename="minty")
 root.title(title)
-# use window_size or 450x900 as default
-root.geometry(window_size)
 
 # Center the window 1/3 of the way up the screen
-window_height = 500
-window_width = 900
+window_height = 800
+window_width = 1000
 screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight() - (root.winfo_screenheight()/3)
 x_cordinate = int((screen_width/2) - (window_width/2))
@@ -192,12 +198,11 @@ menu_bar = Menubutton ( root, text="Menu" )
 menu_bar.menu = Menu ( menu_bar, tearoff = 0 )
 menu_bar["menu"] = menu_bar.menu
 menu_bar.menu.add_command (label="Select File", command=process_file)
+menu_bar.menu.add_command (label="Process all TOML configurations", command=process_all_files)
+menu_bar.menu.add_command (label="Process This Configuration", command=process_this_configuration)
+menu_bar.menu.add_command (label="Process This Configuration", command=clear_parameters)
 menu_bar.menu.add_command (label="Exit", command=root.quit)
 menu_bar.pack(fill="x")
-
-# menu_bar.add_cascade(label="File", menu=file_menu)
-# file_menu.add_command(label="Exit", command=root.quit)
-# file_menu.add_command(label="Open", command=process_file)
 
 # Create a frame to hold the canvas and settings
 frame = Frame(root)
@@ -217,11 +222,52 @@ canvas.create_window((0, 0), window=scrollable_frame, anchor="nw")
 canvas.configure(yscrollcommand=scrollbar.set)
 
 # # Create a frame inside the canvas to hold the settings
-inner_frame = Frame(scrollable_frame)
+inner_frame = Frame(scrollable_frame, width=700)
 inner_frame.pack(side="top", fill="both")
 
-# Add the settings to the inner frame and track changes
-# Add the settings to the inner frame and track changes
-fields = {}
+frame.pack(side="top", fill="both", expand=True)
+canvas.pack(side="top", fill="both", expand=True)
+scrollbar.pack(side="right", fill="y")
+
+# Add some help to the startup screen
+help_text = """
+Welcome to the Benchmark Configuration Editor.  \n\nTo get started, click on the "Select File" option in the menu above.  This will allow you to select a TOML file to edit.  Once you have selected a file, you can make changes to the settings and then click the "Update Above Configuration" button to save your changes.  If you want to save the changes to a new file, enter the new file name in the "New File Path" field and click the "New Configuration using the above path" button.  You can also use the "File" menu to process all TOML configurations in a directory, process the current configuration, or clear the parameters.  Enjoy!
+"""
+
+interframe_width = inner_frame.winfo_width()
+print (f"interframe width: {interframe_width}")
+
+help_frame = Frame(inner_frame, width=100, height=100)
+#help_frame.pack(side="top", fill="both", expand=True, padx=50, pady=50)
+
+help_label = Label(help_frame, text=help_text, wraplength=800, justify="center", font=("Arial", 16))
+#help_label.pack(pady=10, padx=10)
+
+# Create a frame to hold the save buttons
+button_frame = Frame(frame)
+button_frame.pack(side="bottom", pady=10)
+
+original_label = Label(button_frame, text="Original File Path")
+original_label.pack(pady=(10,0))
+
+original_path = StringVar()
+save_path = Label(button_frame, textvariable=original_path, width=100, borderwidth=5, relief="groove")
+save_path.pack()
+
+save_button = Button(button_frame, text="Update Above Configuration", command=save_settings)
+save_button.pack(pady=1)
+
+new_label = Label(button_frame, text="New File Path")
+new_label.pack(pady=(10,0))
+
+new_filename = Text(button_frame, width=100, height=1, relief="groove")
+new_filename.pack(pady=1)
+
+new_button = Button(button_frame, text="New Configuration using the above path", command=new_settings)
+new_button.pack()
+
+# start with the buttons disabled
+new_button["state"] = "disabled"
+save_button["state"] = "disabled"
 
 root.mainloop()
